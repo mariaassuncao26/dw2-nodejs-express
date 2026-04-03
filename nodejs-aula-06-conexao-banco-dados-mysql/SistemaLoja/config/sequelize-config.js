2fb6f4771e14d39f51ccda6da1698670cfd757c2
DSM2 <fatec-dsm2@academico.intra>
2026-04-02 22:25:31 -0300

    Atualização

---
+++ nodejs-aula-06-conexao-banco-dados-mysql/SistemaLoja/config/sequelize-config.js
@@ -0,0 +1,19 @@
+// Importando a ORM Sequelize
+import Sequelize from "sequelize";
+
+// Definindo os dados de conexão com o banco de dados
+const connection = new Sequelize({
+    // Tipo do banco
+    dialect: "mysql",
+    // Endereço do banco
+    host: "localhost",
+    // Nome do usuário do banco
+    username: "root",
+    // Senha
+    password: "",
+    // Fuso horário
+    timezone: "-03:00"
+});
+
+// Exportando o módulo
+export default connection;
