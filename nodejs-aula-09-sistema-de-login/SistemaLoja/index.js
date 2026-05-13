d9754f493f84ed63cb4482a712e86642694463ce
DSM2 <fatec-dsm2@academico.intra>
2026-05-13 20:58:34 -0300

    Atualizado

--- nodejs-aula-09-sistema-de-login/SistemaLoja/index.js
+++ nodejs-aula-09-sistema-de-login/SistemaLoja/index.js
@@ -89,7 +89,7 @@ app.get("/", Auth, function (req, res) {
 });
 
 // INICIA O SERVIDOR NA PORTA 8080
-const port = 8080;
+const port = 8081;
 app.listen(port, function (erro) {
   if (erro) {
     console.log("Ocorreu um erro!");
